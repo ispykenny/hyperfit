69b23433cabb83717677a9919329fc51cc938efb
atokad5 <kenny.krosky@gmail.com>
2019-09-07 16:44:18 -0700

    dynamic form area added.

--- templates/contact.php
+++ templates/contact.php
@@ -81,7 +81,7 @@
           <div class="contact-logo">
             <img src="<?php the_field('logo', 'options') ;?>" alt="">
           </div>
-          <?php echo do_shortcode('[wpforms id="231" title="false" description="false"]'); ?>
+          <?php the_field('form_picker'); ?>
         </div>
         <div class="map map-mobile">
           <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3001.188434325075!2d-73.06512188483529!3d41.21766391446297!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89e87498672933ed%3A0xaa132082bcfd65e!2s88%20Noble%20Ave%20%23105%2C%20Milford%2C%20CT%2006460!5e0!3m2!1sen!2sus!4v1567653168411!5m2!1sen!2sus" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen=""></iframe>
